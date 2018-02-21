5879d7503b329d5b8e337916cb6d770169a711b3
Yusadolat <yusadolat@gmail.com>
2018-02-21 12:20:49 +0000

    Added Header comment

--- merger.sh
+++ merger.sh
@@ -5,7 +5,7 @@ for filename in ./*.csv; do
  if [ "$filename"  != "$OutFileName" ] ;      # Avoid recursion  filename replication
  then 
    if [[ $i -eq 0 ]] ; then 
-      head -1  $filename >   $OutFileName # Copy header if it is the first file
+      head -1  $filename >   $OutFileName # Copy header if it is the first file, This will skip the other header
    fi
    tail -n +2  $filename >>  $OutFileName # Append from the 2nd line each file
    i=$(( $i + 1 ))                        # Increase the counter by 1
